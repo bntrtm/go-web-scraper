5984ae5820feaa322ba2c2f298315bca057b07c7
bntrtm <git.bntrtm@tjbainter.com>
2026-02-15 14:36:34 -0600

    feat: implement crawling

--- get_content.go
+++ get_content.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	gq "github.com/PuerkitoBio/goquery"
 )
@@ -79,4 +84,75 @@ func getImagesFromHTML(html string, baseURL *url.URL) ([]string, error) {
 		extractedURLs = append(extractedURLs, baseURL.ResolveReference(parsedSrc).String())
 	})
 	return extractedURLs, nil
+}
+
+func getHTML(rawURL string) (string, error) {
+	var buf io.Reader
+	client := http.Client{Timeout: time.Second * 10}
+	req, err := http.NewRequest(http.MethodGet, rawURL, buf)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "text/html; charset=utf-8")
+	req.Header.Set("User-Agent", "Crawler/1.0")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("status code %d", resp.StatusCode)
+	}
+	if ct := resp.Header.Get("Content-Type"); !strings.HasPrefix(ct, "text/html") {
+		return "", fmt.Errorf("expected content-type text/html, got %s", ct)
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	html := string(bytes)
+	return html, nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	baseHostname := baseURL.Hostname()
+	curURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentHostname := curURL.Hostname()
+	if baseHostname != currentHostname {
+		return
+	}
+
+	normCurrent, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, ok := pages[normCurrent]; ok {
+		pages[normCurrent] += 1
+		return
+	} else {
+		pages[normCurrent] = 1
+	}
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		log.Printf("error: %s\n", err)
+	}
+	fmt.Println("______________________________")
+	fmt.Printf("SCRAPING LINKS FROM HTML: %s\n", rawCurrentURL)
+	fmt.Println("______________________________")
+	// fmt.Println(html)
+	urls, err := getURLsFromHTML(html, curURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, url := range urls {
+		crawlPage(rawBaseURL, url, pages)
+	}
 }
--- main.go
+++ main.go
@@ -14,4 +14,11 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("starting crawl of: %s\n", os.Args[1])
+	pages := map[string]int{}
+	crawlPage(os.Args[1], os.Args[1], pages)
+	fmt.Println("HTML Extracted:")
+	fmt.Println("____________________")
+	for k, v := range pages {
+		fmt.Printf("%s: %d\n", k, v)
+	}
 }
